dae7f86a8d1e0433f5e065e6e9d95a844ab5fe69
shashidhar <shashidhar.shivaraj715@gmail.com>
2023-07-23 17:37:23 +0530

    final commit

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mongoose=require('mongoose');
-const mongoURI='mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+// const mongoURI='mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+const mongoURI='mongodb+srv://shashidhar:MbGTOscSuaewg68W@cluster0.2jvaary.mongodb.net/?retryWrites=true&w=majority';
+// mongodb+srv://shashidhar:MbGTOscSuaewg68W@cluster0.2jvaary.mongodb.net/?retryWrites=true&w=majority
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI)
@@ -12,3 +14,6 @@ const connectToMongo=()=>{
 }
 
 module.exports=connectToMongo;
+// MbGTOscSuaewg68W
+// mongodb+srv://shashidhar:<password>@cluster0.ysffrah.mongodb.net/?retryWrites=true&w=majority
+// mongodb+srv://shashidhar:MbGTOscSuaewg68W@cluster0.ysffrah.mongodb.net/?retryWrites=true&w=majority
